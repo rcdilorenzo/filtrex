2e7564e39044b246ad02217a2b9ee1959d9cde65
Christian Di Lorenzo <rcdilorenzo@me.com>
2016-02-26 17:57:45 -0500

    Bump to 0.2.0-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Filtrex.Mixfile do
 
   def project do
     [app: :filtrex,
-     version: "0.1.0",
+     version: "0.2.0-dev",
      elixir: "~> 1.2",
      description: description,
      package: package,
