430f24713a86159676f6a77277817c24f0b26615
Daniel Berkompas <himself@danielberkompas.com>
2016-06-27 11:47:50 -0700

    Add case insensitivity for LIKE queries (#28)
    * Add case insensitivity for LIKE queries
    
    Usually, you want your LIKE queries to be case insensitive. Even though it does incur a modest performance hit, it makes the LIKE query much more likely to return proper results.
    
    For example, if you're searching for any records that include "Hello", it's nice to be able to type "Hello", and get results for "hello" as well.
    
    * Fix broken tests

--- test/ast_test.exs
+++ test/ast_test.exs
@@ -16,7 +16,7 @@ defmodule FiltrexASTTest do
     ast = Filtrex.AST.build_query(Filtrex.SampleModel, @filter)
     expression = Macro.to_string(quote do: unquote(ast))
     assert with_newline(expression) == """
-    Ecto.Query.where(Filtrex.SampleModel, [s], fragment("((title LIKE ?) OR (title != ?)) OR ((date_column > ?) AND (date_column < ?))", "%created%", "Chris McCord", "2016-05-01", "2017-01-01"))
+    Ecto.Query.where(Filtrex.SampleModel, [s], fragment("((lower(title) LIKE lower(?)) OR (title != ?)) OR ((date_column > ?) AND (date_column < ?))", "%created%", "Chris McCord", "2016-05-01", "2017-01-01"))
     """
   end
 
